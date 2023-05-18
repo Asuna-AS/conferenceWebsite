1bb6840f9855b6d32b29f41ce9a75b87f8ee1afe
Abhishek Hazra <30319288+abirhazra@users.noreply.github.com>
2023-05-18 12:42:40 +0530

    updated

--- conferencewebsite/src/components/ImpDates.js
+++ conferencewebsite/src/components/ImpDates.js
@@ -4,11 +4,11 @@ export const ImpDates = () => {
   return (
     <div className='flex flex-col px-36 pt-16 text-left'>
       <p>
-      <b>Submission open:</b> December 1st, 2022 <br></br>
-      <b>Paper submission Deadline:</b> January 15th, 2023 January 31st, 2023<br></br>
-      <b>Acceptance notification:</b> February 19th, 2023<br></br>
-      <b>Camera-ready submission:</b> March 17th, 2023<br></br>
-      <b>Conference Dates:</b> 14<sup>th</sup>-17<sup>th</sup> November, 2023
+      <b>Submission open:</b> Jan 1st, 2023 <br></br>
+      <b>Paper submission Deadline:</b> January 15th, 2023 January 31st, 2023.<br></br>
+      <b>Acceptance notification:</b> February 19th, 2023.<br></br>
+      <b>Camera-ready submission:</b> March 17th, 2023.<br></br>
+      <b>Conference Dates:</b> 14<sup>th</sup>-17<sup>th</sup> November, 2023.
       </p>
     </div>
   )
