9d29a8565fe073e1145f4f020dd2756288656fc4
Arunim singhal <75484060+arunim-iiitlCS@users.noreply.github.com>
2023-05-19 13:52:17 +0530

    update

--- conferencewebsite/src/components/Committee.js
+++ conferencewebsite/src/components/Committee.js
@@ -2,6 +2,40 @@ import React from 'react'
 
 export const Committee = () => {
   return (
-    <div>Committee</div>
+    <div className='flex flex-col text-left px-36 py-10 gap-5'>
+      <h1 className='text-gray-500 text-xl font-semibold'>DMLEFC Chairs</h1>
+      <p className='text-gray-500'>
+      Dr. Mainak Adhikari, Head of Department and Assistant Professor, Indian Institute of Information Technology Lucknow, India. (Email: mainak@iiitl.ac.in)
+      <br></br>Dr. Amit Kumar Singh, Department of Computer Science and Engineering, National Institute of Technology Patna, India. (Email: amit.singh@nitp.ac.in )
+      <br></br>Dr. Abhishek Hazra, Communications & Networks Lab, Department of Electrical and Computer Engineering, National University of Singapore, Singapore, (Email: a.hazra@ieee.org).
+      </p>
+      <h1 className='text-gray-500 text-xl font-semibold'>Publicity Chair</h1>
+      <p className='text-gray-500'>
+      Dr. Sudarshan Nandy, School Computer Science and Engineering (SCOPE), VIT University, Vellore, Contact: sudarshannandy@gmail.com
+      </p>
+      <h1 className='text-gray-500 text-xl font-semibold'>Publication Chair</h1>
+      <p className='text-gray-500'>
+      Dr. Varun G. Menon, IEEE Senior Member,  Professor at SCMS School of Engineering and Technology, Kerala, India. Contact: varungmenon46@gmail.com
+      </p>
+      <h1 className='text-gray-500 text-xl font-semibold'>Web Master</h1>
+      <p className='text-gray-500'>
+      Arunim Singhal, Indian Institute of Information Technology, Lucknow, India. (Email: arunimsinghal64@gmail.com)
+      </p>
+      <h1 className='text-gray-500 text-xl font-semibold'>Program Committee Members (tentative)</h1>
+      <p className='text-gray-500'>
+      Dr. Ali Kashif Bashir, Manchester Metropolitan University, United Kingdom
+      <br></br>Prof. Shahid Mumtaz, Instituto de Telecomunicações, Portugal 
+      <br></br>Prof. Haris Gačanin, IEEE Fellow, RWTH Aachen University, Germany
+      <br></br>Prof. Danda B Rawat, Howard University, USA
+      <br></br>Prof. Ajith Abraham, Machine Intelligence Research Labs (MIR Labs), USA
+      <br></br>Prof. Guangjie Han, Hohai University, China
+      <br></br>Dr. Venki Balasubramaniam, Federation University, Australia 
+      <br></br>Dr. Indranil Sarkar, University of Oslo, Norway
+      <br></br>Dr. Praveen Kumar Donta, Distributed Systems Group, TU Wien, Austria
+      <br></br>Dr. Sahil Garg, École de technologie supérieure, Université du Québec, Canada   
+      <br></br>Dr. Alakesh Kalita, NUS Singapore
+      <br></br>Dr. Satish Narayana Srirama, University of Hyderabad  
+      </p>
+    </div>
   )
 }
--- conferencewebsite/src/components/Program.js
+++ conferencewebsite/src/components/Program.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Program = () => {
   return (
-    <div className='text-black'>Program</div>
+    <div className='px-36 py-10'>
+      <h1 className='text-gray-500 text-xl font-semibold'>Program</h1>
+      <br></br>
+      <p className='text-gray-500'>TBD</p>
+    </div>
   )
 }
